00ccb5c7a4057f7b22e05226c21f799b1fef1e0c
FaridBash <farid.bashiti@gmail.com>
2023-02-01 12:47:30 +0200

    import fix

--- app.js
+++ app.js
@@ -2,14 +2,19 @@
 import { Monster} from "./monster.js";
 import{players} from "./player.js";
 
-
+let monsterTurn=false;
 
 
 const attackBtn=document.getElementsByClassName('attack');
 
+function whoIsAttacking(){
+    if (Monster.monsterDexterity>players.dexterity) monsterTurn=true;
+    else if(Monster.monsterDexterity<players.dexterity) monsterTurn=false;
+}
 
 attackBtn[0].addEventListener('click', e=>{
     alert(Monster.monsterDexterity);
+    alert(players.dexterity);
     console.log('atack');
 
 });
