15c0c8c2047378e15846fc5dc40777568d573ee1
T1To48 <tofek.98@gmail.com>
2023-02-01 13:51:00 +0200

    123

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ import{players} from "./player.js";
 
 let monsterTurn=false;
 
-
+let attackerProfile=monsterTurn===false?players:Monster;
+let victimProfile=monsterTurn===true?players:Monster;
 const attackBtn=document.getElementsByClassName('attack');
 
 function whoIsAttacking(){
@@ -16,6 +17,48 @@ attackBtn[0].addEventListener('click', e=>{
     alert(Monster.monsterDexterity);
     alert(players.dexterity);
     console.log('atack');
-
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function damageCalc(){
+   return((randomNumbGenerator()+attackerProfile.strength)*attackerProfile.level)-attackerProfile.defence;
+}
+
+function attack(){
+   if(damageCalc>0){
+      return victimProfile.healthPoints-=damageCalc();
+   }
+}
+
+function winner(){
+   if (victimProfile.healthPoints<=0){
+
+   }
+   //else switch turns..
+}
+
+
+//Location names Array
+let gameArena=['desert','castle','Fortress','waterfall','atlantis']
+//Match each location with random number of monsters between 5-10.
+function random5_10(){
+   return Math.floor(Math.random()*10)+5;
+}
