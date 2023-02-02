e9f9394f4b7a9a6c5ad55353d50acc09621f81fa
FaridBash <farid.bashiti@gmail.com>
2023-02-02 11:32:00 +0200

    sep js files

--- test.js
+++
@@ -1,9 +0,0 @@
-const ul=document.querySelector('#ul');
-let x=24.16;
-
-
-
-ul.innerHTML=`<li id="testing">${x}</li>`;
-const testing=document.querySelector('#testing');
-if(testing.textContent==24.16){
-  console.log('possible')};
